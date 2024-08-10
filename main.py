2877d0e8ed442972fe025c8645d80ead40df2062
MaksimSigma228 <maksimbezbatcenko5@gmail.com>
2024-08-10 20:37:44 +0300

    init

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
 from kivy.uix.scrollview import ScrollView
-from instruction import txt_instruction, txt_test1, txt_test2, txt_test3, txt_sits
+from instructions import txt_instruction, txt_test1, txt_test2, txt_test3, txt_sits
 from ruffier import test
 
 
